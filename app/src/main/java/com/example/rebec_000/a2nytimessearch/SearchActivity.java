bf47b97520e348bdef747e3623eab5dad08afa74
Rebecca Bazile <rebeccaliggie@gmail.com>
2018-07-17 19:01:50 -0400

    firstNYTime

---
+++ app/src/main/java/com/example/rebec_000/a2nytimessearch/SearchActivity.java
@@ -0,0 +1,101 @@
+package com.example.rebec_000.a2nytimessearch;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import cz.msebera.android.httpclient.Header;
+
+public class SearchActivity extends AppCompatActivity {
+    EditText etQuery;
+    Button btnSearch;
+    GridView gvResults;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        setupViews();
+        }
+
+    public void setupViews() {
+        etQuery= (EditText) findViewById(R.id.etQuery);
+        btnSearch=(Button) findViewById(R.id.btnSearch);
+        gvResults= (GridView) findViewById(R.id.gvResults);
+
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onArticleSearch(View view) {
+        String query = etQuery.getText().toString();
+
+        Toast.makeText(this,"searching for" + query,Toast.LENGTH_LONG);
+
+        AsyncHttpClient client= new AsyncHttpClient();
+        String url ="https://api.nytimes.com/svc/search/v2/articlesearch.json";
+
+        RequestParams params= new RequestParams();
+        params.put("api-key", "4be5f2fbfb364ea895c91d32830487b2");
+        params.put("page", 0);
+        params.put("q", query);
+
+        client.get(url, params, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                Log.d("DEBUG", response.toString());
+                JSONArray articleJsonResults = null;
+                try {
+                    articleJsonResults = response.getJSONObject("resonse").getJSONArray("docs");
+                    Log.d("DEBUG", articleJsonResults.toString());
+                }catch(JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+    }
+}
